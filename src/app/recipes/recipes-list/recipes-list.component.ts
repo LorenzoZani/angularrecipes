7d8c5e4a02a0ac2704edc15c333a971b91d65208
Lorenzo <lorenzozani1995@gmail.com>
2018-12-04 17:24:32 +0100

    Added Recipe
    added RecipeList and various recipes

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "./recipe.model";
 @Component({
-  selector: 'app-recipes-list',
-  templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.scss']
+  selector: "app-recipes-list",
+  templateUrl: "./recipes-list.component.html",
+  styleUrls: ["./recipes-list.component.scss"]
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Patate riso e cozze",
+      "Lorem ipsum",
+      "https://ips.plug.it/cips/buonissimo.org/cms/2012/04/tiella-di-riso-patate-e-cozze.jpg"
+    ),
+    new Recipe(
+      "Carbonara",
+      "Ipsum lorem",
+      "https://cc-media-foxit.fichub.com/image/fox-it-life/3632f012-c656-4010-bb31-4d2c379aef5d/spaghetti-carbonara-1200x630.jpg"
+    ),
+    new Recipe(
+      "Diavola",
+      "Lorem Pizza",
+      "http://www.planetpizzaal.it/castelletto/wp-content/uploads/pizza-diavola.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
