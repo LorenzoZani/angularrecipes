287e32b930532874b9b29ed29e633670d22f1ce9
Lorenzo <lorenzozani1995@gmail.com>
2018-12-07 13:30:32 +0100

    Implemented search button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { RecipesService } from "./../recipes.service";
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  searchTerm = new FormControl("cake");
+  constructor(private service: RecipesService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  submitSearch() {
+    this.service.searchRecipes(this.searchTerm.value);
   }
-
+  ngOnInit() {}
 }
